fba8e2e6380a54e7c3186ecbcd524d0cf9d1506f
julianteske <julian.teske@rwth-aachen.de>
2020-04-28 19:44:50 +0200

    WIP: write setup.py to package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='qsim',
+    version='0.1',
+    packages=['qsim'],
+    url='',
+    license='GLP3',
+    author='Julian Teske',
+    author_email='j.teske@fz-juelich.de',
+    description='Optimal Control for Quantum Systems'
+)
