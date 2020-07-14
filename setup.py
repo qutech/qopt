e535c04ad25ccaaebbe7cce4a1ba6842673acf11
INSTITUT2B\teske <julian.teske@rwth-aachen.de>
2020-07-14 11:44:11 +0200

    WIP: readthedocs

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description='Optimal Control for Quantum Systems',
     package_dir={'qopt': 'qopt'},
     install_requires=['numpy', 'scipy', 'matplotlib', 'cython', 'nose',
-                      'jupyter notebook', 'simanneal', 'pandas', 'spyder',
+                      'simanneal', 'pandas', 'spyder',
                       'filter_functions', 'qutip', 'opt_einsum', 'sparse'],
     extras_require={
         'fancy_progressbar': ['tqdm', 'requests'],
