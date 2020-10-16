80ec459d473e1473cc42abe1185aaccdde3be88c
Julian Teske <teske@institut2b.physik.rwth-aachen.de>
2020-10-16 13:39:21 +0200

    implement the parallelization in a function

--- qopt/parallel.py
+++ qopt/parallel.py
@@ -1,18 +1,7 @@
 from multiprocessing import Pool
 import copy
 
-
-class ParallelOptimizer(object):
-
-    def __init__(self, simulator, optimizer, processes = None):
-        self.simulator = simulator
-        self.optimizer = optimizer
-        self.processes = processes
-
-    def parallelize_optimization(self, initial_pulses):
-        with Pool(processes=self.processes) as pool:
-            results = pool.map(self.optimizer.run_optimization, initial_pulses)
-        return results
+from qopt.data_container import DataContainer
 
 
 def run_optimization(optimizer, initial_pulse):
@@ -24,4 +13,7 @@ def run_optimization_parallel(optimizer, initial_pulses, processes=None):
     with Pool(processes=processes) as pool:
         results = pool.starmap(
             run_optimization, zip(optimizers, initial_pulses))
-    return results
+    data = DataContainer()
+    for result in results:
+        data.append_optim_result(result)
+    return data
