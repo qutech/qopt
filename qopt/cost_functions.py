- adds a todo for the simulator - fixes a small bug in the state fidelity derivative - fixes a bug in the OversamplingTF occuring when no boundaries are added

qopt/cost_functions.py
@@ -485,7 +485,7 @@ def derivative_state_fidelity(
             # here we need to take the real part.
             if computational_states:
                 derivative_fidelity[t, ctrl] = 2 * np.real(
-                    scalar_prod * (
+                    (scalar_prod * (
                             target * (
                             reversed_propagators[::-1][t + 1]
                             * propagator_derivatives[ctrl][t]
@@ -494,7 +494,7 @@ def derivative_state_fidelity(
                         subspace_indices=computational_states,
                         map_to_closest_unitary=rescale_propagated_state
                     )
-                    )[0, 0])
+                    ))[0, 0])
             else:
                 derivative_fidelity[t, ctrl] = 2 * np.real(
                     (scalar_prod * (target * reversed_propagators[::-1][t + 1]

qopt/transfer_function.py
@@ -587,8 +587,12 @@ class OversamplingTF(TransferFunction):
         padding_start, padding_end = self.num_padding_elements
 
         # deriv_by_ctrl_amps: shape (num_x, num_f, num_par)
-        cropped_derivs = deriv_by_transferred_par[
-                         padding_start:-padding_end, :, :]
+        if padding_end > 0:
+            cropped_derivs = deriv_by_transferred_par[
+                             padding_start:-padding_end, :, :]
+        else:
+            cropped_derivs = deriv_by_transferred_par[
+                             padding_start:, :, :]
 
         cropped_derivs = np.expand_dims(cropped_derivs, axis=1)
         cropped_derivs = np.reshape(