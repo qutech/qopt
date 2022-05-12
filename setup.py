aeeceec81b3ce6d09e88ac67fa0288792bc00021
Julian Teske <julian.teske@rwth-aachen.de>
2022-05-12 17:27:51 +0200

    install pytest-cov for tests, delete the qopt_tests.__init__ it does not have to be a package of its own,

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='qopt',
     version='1.3',
-    packages=['qopt', 'qopt_tests'],
+    packages=['qopt'],
     url='https://github.com/qutech/qopt',
     license='GLP3',
     author='Julian Teske',
@@ -14,7 +14,7 @@ setup(
     extras_require={
         'doc': ['ipython', 'ipykernel', 'nbsphinx', 'numpydoc', 'sphinx',
                 'jupyter_client', 'sphinx_rtd_theme'],
-        'qopt_tests': ['pytest', ],
+        'qopt_tests': ['pytest', 'pytest_cov'],
     },
     test_suite='qopt_tests',
     classifiers=[
